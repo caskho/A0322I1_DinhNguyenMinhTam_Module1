317ecdced262393b04d94cda510c0f0989a7af1b
caskho <dinhnguyenminhtam@icloud.com>
2022-05-16 17:50:05 +0700

    ff

---
+++ ss15_ham2/bai tap/main.js
@@ -0,0 +1,11 @@
+function isPrime(number) {
+    alert (prompt ('nhap so nguyen to'))
+    if(number > 1, number <1000) {
+        alert ('day la so nguyen to')
+    } else {
+       number >1000,  number < 1, alert ('day khong phai la mot so nguyen to');
+
+    }
+
+}
+isPrime ();
